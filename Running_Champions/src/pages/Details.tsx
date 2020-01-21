c0b7eccda81319ec54adb173c4676ceee146b9f9
R-Slezak <rob.slezak@live.com>
2020-01-21 17:51:20 -0500

    Adding details page plus small tweaks

--- Running_Champions/src/pages/Details.tsx
+++ Running_Champions/src/pages/Details.tsx
@@ -1,7 +1,10 @@
-import { IonBackButton, IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonCard, IonButton, IonBackButton, IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
+import '../css/Details.css';
 
 const DetailsPage: React.FC = () => {
+  const run = { name: "Run 1", time: 10, date: "01/01/2020 1:00pm", distance: 1.0 };
+
   return (
     <IonPage>
       <IonHeader>
@@ -16,7 +19,30 @@ const DetailsPage: React.FC = () => {
         </IonToolbar>
       </IonHeader>
 
-      <IonContent>
+      <IonContent id="details">
+          <section className="details-section">
+            <div className="item">
+                <span className="item-label">Name:</span>
+                <span className="item-data">
+                    <input defaultValue={ run.name } /> 
+                    <IonButton onClick={(e) => console.log('save button')}>Save</IonButton>
+                </span>
+            </div>
+            <div className="item">
+                <span className="item-label">Duration:</span>
+                <span className="item-data">{ run.time } min</span>
+            </div>
+            <div className="item">
+                <span className="item-label">Distance:</span>
+                <span className="item-data">{ run.distance } km</span>
+            </div>
+            <div className="item">
+                <span className="item-label">Date:</span>
+                <span className="item-data">{ run.date }</span>
+            </div>
+          </section>
+          
+          <IonCard className="map-placeholder">Insert Map Here</IonCard>
       </IonContent>
     </IonPage>
   );
