e2b0bdcac187451fff28a0f5e157636fe216f45a
ElderNobrega <eldernguerra.filho@hotmail.com>
2020-02-10 12:08:33 -0500

    Setting up firebase

---
+++ Running_Champions/src/components/firebaseConfig.ts
@@ -0,0 +1,31 @@
+import * as fb from 'firebase'
+
+const config = {
+
+    /*
+
+        <!-- The core Firebase JS SDK is always required and must be listed first -->
+        <script src="https://www.gstatic.com/firebasejs/7.8.1/firebase-app.js"></script>
+
+        <!-- TODO: Add SDKs for Firebase products that you want to use
+        https://firebase.google.com/docs/web/setup#available-libraries -->
+        <script src="https://www.gstatic.com/firebasejs/7.8.1/firebase-analytics.js"></script>
+    */
+    apiKey: "AIzaSyAgLMcPKBRwnknPS7Zefx-ZFIWGUQQwF9s",
+    authDomain: "running-champions-96792.firebaseapp.com",
+    databaseURL: "https://running-champions-96792.firebaseio.com",
+    projectId: "running-champions-96792",
+    storageBucket: "running-champions-96792.appspot.com",
+    messagingSenderId: "441831629749",
+    appId: "1:441831629749:web:832fc033dec8d93a58affc",
+    measurementId: "G-X9XPTWBRMY"
+}
+
+fb.initializeApp(config)
+//firebase.analytics();
+
+export async function loginUser(username: string, password: string) {
+    // Authentication with firebase
+    // if present, show home
+    //if not, show error msg 
+}
