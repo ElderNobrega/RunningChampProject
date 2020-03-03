e168c211d50e1bdd88deab0f4954fab0853c606f
R-Slezak <rob.slezak@live.com>
2020-03-03 14:48:17 -0500

    helper function to check if user is logged in

--- Running_Champions/src/redux/actions.ts
+++ Running_Champions/src/redux/actions.ts
@@ -1,3 +1,11 @@
+import { useSelector } from "react-redux";
+
 export const setUserState = (payload: any) => {
     return { type: 'SET_USER_STATE', payload}
+}
+
+export const userIsLoggedIn = () => {
+    return useSelector((state: any) => {
+        return state !== undefined;
+    })
 }
