559a825cb4c0890f3de7ce7f511631925fcc5c77
ElderNobrega <eldernguerra.filho@hotmail.com>
2020-03-03 15:54:20 -0500

    Comment useSelector

--- Running_Champions/src/redux/actions.ts
+++ Running_Champions/src/redux/actions.ts
@@ -1,11 +1,12 @@
-import { useSelector } from "react-redux";
+//import { useSelector } from "react-redux";
 
 export const setUserState = (payload: any) => {
     return { type: 'SET_USER_STATE', payload}
 }
 
-export const userIsLoggedIn = () => {
+/*export const userIsLoggedIn = () => {
     return useSelector((state: any) => {
         return state !== undefined;
     })
 }
+*/
